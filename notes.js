3bea8303d37982794c97a6495fe46bba4791e1d4
araju <Ashwin.Raju@twc-contractor.com>
2018-04-30 21:59:16 -0600

    message

---
+++ notes.js
@@ -0,0 +1,58 @@
+console.log('Starting notes.js');
+const fs = require('fs');
+
+/*
+Method to fetch notes.
+*/
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch(e) {
+     return []
+  }
+};
+
+/*
+Method to save note.
+*/
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+/*
+Method to add note.
+*/
+var addNote = (title, body) => {
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+
+ var duplicateNotes = notes.filter((note) => note.title === title);
+ console.log("duplicateNotes : " + duplicateNotes);
+ if(duplicateNotes.length === 0){
+   notes.push(note);
+   saveNotes(notes);
+   return note;
+ }
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Getting note: ', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note: ', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,removeNote
+};
